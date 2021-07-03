5ed9bb9ec2be60dbe17bdbeef247244712395fa0
maximosan <maximosantoro@protonmail.com>
2021-07-03 14:57:35 -0300

    do db model, and migrate

--- todo/dos/models.py
+++ todo/dos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Do(models.Model):
+    title = models.CharField(max_length = 128)
+    completed = models.BooleanField(default = False)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
